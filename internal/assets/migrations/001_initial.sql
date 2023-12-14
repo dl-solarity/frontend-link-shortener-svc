5effb7fb7fe26bdf0681944c6465d3fa64ba2023
Danylo Ostapchenko <danya.ostap@gmail.com>
2023-12-14 15:21:01 +0200

    Add initial migration

--- internal/assets/migrations/001_initial.sql
+++ internal/assets/migrations/001_initial.sql
@@ -1,3 +1,12 @@
 -- +migrate Up
 
+CREATE TABLE links (
+    id char(64) primary key,
+    path text NOT NULL,
+    value jsonb NOT NULL,
+    created_at timestamp without time zone
+);
+
 -- +migrate Down
+
+DROP TABLE IF EXISTS links;
